00e5d0257b6c984ea31a9e3b2d78f900c50ad64d
cat chu <chu.catherine91@gmail.com>
2024-03-30 14:51:12 +0000

    week 2 readme

--- greenery/models/marts/core/fact_orders.sql
+++ greenery/models/marts/core/fact_orders.sql
@@ -5,24 +5,29 @@
 }}
 
 SELECT
-  o.order_id,
-  o.order_placed_at,
-  o.order_total,
-  o.order_status,
-  o.promo_code_id,
-  pc.promo_code_discount,
-  o.user_id,
-  u.first_name AS user_first_name,
-  o.tracking_id as shipment_tracking_id,
-  s.status AS shipment_status,
-  DATEDIFF(day, o.order_placed_at, o.delivered_at) AS days_to_deliver,
-  oa.order_id IS NOT NULL AS has_adjustment
+    o.order_id
+  , o.order_placed_at
+  , o.order_total
+  , o.order_cost
+  , o.status as order_status
+  , o.promo_code_id
+  , pc.promo_code_discount
+  , o.user_id
+  , concat(u.first_name, ' ', u.last_name) AS user_name
+  , o.address_id
+  , a.address
+  , a.zipcode
+  , a.state
+  , a.country
+  , o.shipping_service
+  , o.tracking_id as shipment_tracking_id
+  , o.shipping_cost
+  , o.estimated_delivery_at
+  , DATEDIFF(day, o.order_placed_at, o.delivered_at) AS days_to_deliver
 FROM {{ ref('stg_orders') }} o
 LEFT JOIN {{ ref('stg_promo_codes') }} pc
-  ON o.promo_code_id = pc.promo_code_id
+    ON o.promo_code_id = pc.promo_code_id
 LEFT JOIN {{ ref('stg_users') }} u
-  ON o.user_id = u.user_id
-LEFT JOIN {{ ref('stg_shipments') }} s
-  ON o.shipment_id = s.shipment_id
-LEFT JOIN {{ ref('stg_order_adjustments') }} oa
-  ON o.order_id = oa.order_id
+    ON o.user_id = u.user_id
+LEFT JOIN {{ ref('stg_addresses' )}} a
+    ON o.address_id = a.address_id
